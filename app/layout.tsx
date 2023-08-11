61a4ff868faaa2c04b599d84066004e952337a6e
tunogya <tunogya@qq.com>
2023-08-11 11:03:43 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import "tailwindcss/tailwind.css"
 import {Providers} from './providers';
 import '@rainbow-me/rainbowkit/styles.css';
-import Script from "next/script";
 
 function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
-    <Script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="NESTFiBot" data-size="large"
-            data-onauth="onTelegramAuth(user)"></Script>
     <body>
     <Providers>{children}</Providers>
     </body>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BOT_TOKEN: process.env.BOT_TOKEN
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
